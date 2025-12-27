aec9c2141a95386bbbb57690544b5cfc4cc4e9c4
codewebs2025 <maidanagutierrezjuanpablo11@gmail.com>
2025-12-27 11:51:05 -0300

    Update script.js

--- panaderia-juan/panaderia-juan/panaderia-juan/script.js
+++ panaderia-juan/panaderia-juan/panaderia-juan/script.js
@@ -1,1 +1,13 @@
+// Cuando la página carga
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Sitio cargado correctamente");
 
+  // Botón WhatsApp (si existe)
+  const boton = document.querySelector(".btn");
+
+  if (boton) {
+    boton.addEventListener("click", () => {
+      console.log("Click en botón de contacto");
+    });
+  }
+});
